65e4f9486653856417490129259513eee32ab5b3
Colin King <colin.king@segment.com>
2018-10-12 15:30:20 -0700

    Add Typescript declaration

---
+++ types/index.d.ts
@@ -0,0 +1,16 @@
+/// <reference types="jest" />
+
+import { DiffOptions } from 'jest-diff';
+
+declare type FileMatcherOptions = {
+  diff: DiffOptions
+}
+
+declare namespace jest {
+  interface Matchers<R> {
+    toMatchFile: (output: string, filename: string, options?: FileMatcherOptions) => void
+  }
+   interface Expect {
+    toMatchFile: (output: string, filename: string, options?: FileMatcherOptions) => void
+  }
+}
